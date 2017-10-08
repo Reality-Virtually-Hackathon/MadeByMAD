79318745be9a2d2e381b1f94ac645b5e6516ed0b
Mario Dcunha <mario.dcn@gmail.com>
2017-10-08 15:40:56 -0400

    commit-2

--- js/javascript.js
+++ js/javascript.js
@@ -1,29 +1,61 @@
+// import 'aframe'
+// import 'aframe-mouse-cursor-component'
 
 var wonderTitle = ['Petra, Jordan', 'The Taj Mahal, India', 'The Colossuem, Italy'];
 
 var wonderSky = ['https://farm6.static.flickr.com/5082/5209434257_19baa41784_b.jpg', 'https://c1.staticflickr.com/1/59/231140890_babadb19ef_b.jpg', 'https://c1.staticflickr.com/3/2600/4105964603_f4893ee9a0_b.jpg'];
 
-var builtBy = ['The Nabataeans', 'Emperor Shah Jahan', 'Titus'];
-var builtWhen = ['5th Cent.B.C', '1653', '80 AD'];
+var builtBy = ['Built By The Nabataeans', 'Built By Emperor Shah Jahan', 'Built By Titus'];
+var builtWhen = ['Built in 5th Cent.B.C', 'Built in 1653', 'Built in 80 AD'];
 var voice = ['src: #wonder-0', 'src: #wonder-1', 'src: #wonder-2'];
 
+var choice;
 
-function choose(array)
+$(document).ready(choose());
+// $(document).addEventListener('click', chooseOnly());
+
+function chooseOnly()
 {
-  return Math.floor(Math.random()*array.length);
+	choice = Math.floor(Math.random()*3);
 }
 
-var choice = choose(wonderTitle);
 
-selectedWonderTitle = wonderTitle[choice];
-selectedWonderSky = wonderSky[choice];
-selectedVoice = voice[choice];
+//$(document).click(choose());
 
-console.log(selectedVoice);
+// var choice = Math.floor(Math.random()*3);
+
+// selectedWonderTitle = wonderTitle[choice];
+// selectedWonderSky = wonderSky[choice];
+// selectedVoice = voice[choice];
+// selectedBuiltBy = builtBy[choice];
+// selectedBuiltWhen = builtWhen[choice];
+
+// $('a-sky').attr('src', selectedWonderSky);
+// $('#wonder-title').attr('value', selectedWonderTitle);
+// $('#built-by').attr('value', selectedBuiltBy);
+// $('#built-when').attr('value', selectedBuiltWhen);
+
+function choose()
+{
+	choice = Math.floor(Math.random()*3);
+
+	selectedWonderTitle = wonderTitle[choice];
+	selectedWonderSky = wonderSky[choice];
+	selectedVoice = voice[choice];
+	selectedBuiltBy = builtBy[choice];
+	selectedBuiltWhen = builtWhen[choice];
+
+	$('a-sky').attr('src', selectedWonderSky);
+	$('#wonder-title').attr('value', selectedWonderTitle);
+	$('#built-by').attr('value', selectedBuiltBy);
+	$('#built-when').attr('value', selectedBuiltWhen);
+
+	console.log(choice);
+}
 
 
-$('a-text').attr('value', selectedWonderTitle);
-$('a-sky').attr('src', selectedWonderSky);
+
+
 
 //var playSound = $('#sky');
 //playSound.play();
